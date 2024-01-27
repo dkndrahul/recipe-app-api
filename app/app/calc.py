29775abe6465fa56a6905e42b0e4cc97a958b431
dkndrahul <dkndrahul@live.com>
2024-01-27 11:42:32 -0500

    addin psycopg

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """Add x and y return result """
+    return x + y
+
+def substract(x, y):
+    """Substract x and y return result """
+    if x < y :
+        return y - x
+    else :
+        return x - y
